cd342d1494dcca100bd177fdf6d3425e0254d8c5
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-02-15 09:15:48 +0400

    Initialized an empty project and also added IDE specific files to ignore list

---
+++ Dashboard.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Upper Camel Case naming convention
+public class Dashboard {
+    public static void main(String[] args) throws InterruptedException {
+        // Declare
+    }
+}
